facd6e4357d548704743cb4f7117f9992ba9b34e
Kevin Heredia <53192568+k-v-n-h@users.noreply.github.com>
2020-08-27 13:58:46 -0600

    Update copyassignments.py
    simple script to organize the desktop adjust accordingly

--- copyassignments.py
+++ copyassignments.py
@@ -1,13 +1,15 @@
 import os
 import shutil
 
+# simple script to organize the desktop 
+
 def stu_activities():
-    for dir in os.listdir("/Users/kevinheredia/Desktop"):
+    for dir in os.listdir("/Users/Desktop"):
         assignments = "assignments"
         if os.path.exists(assignments) != True:
             os.mkdir(assignments)
         if dir.startswith("Stu_"):
-            path = os.path.join("/Users/kevinheredia/Desktop", dir)
+            path = os.path.join("/Users/Desktop", dir)
             shutil.move(path, assignments)
             
 
